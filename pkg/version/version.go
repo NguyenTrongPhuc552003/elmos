a2a6c7b7986516cdffc04bc90ec2fbccb14a87b0
Phuc Nguyen <trong552003@gmail.com>
2025-12-31 23:20:10 +0700

    pkg: version: add version package
    - Define Version, Commit, BuildDate variables for ldflags
    - Add GetVersionInfo() for formatted version string
    - Support build-time version injection
    
    Signed-off-by: Phuc Nguyen <trong552003@gmail.com>

---
+++ pkg/version/version.go
@@ -0,0 +1,54 @@
+// Package version provides version information for the elmos CLI.
+package version
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Build information (set via ldflags at build time)
+var (
+	Version   = "dev"
+	Commit    = "unknown"
+	BuildDate = "unknown"
+)
+
+// Info holds version information
+type Info struct {
+	Version   string `json:"version"`
+	Commit    string `json:"commit"`
+	BuildDate string `json:"buildDate"`
+	GoVersion string `json:"goVersion"`
+	OS        string `json:"os"`
+	Arch      string `json:"arch"`
+}
+
+// Get returns the current version information
+func Get() Info {
+	return Info{
+		Version:   Version,
+		Commit:    Commit,
+		BuildDate: BuildDate,
+		GoVersion: runtime.Version(),
+		OS:        runtime.GOOS,
+		Arch:      runtime.GOARCH,
+	}
+}
+
+// String returns a formatted version string
+func (i Info) String() string {
+	return fmt.Sprintf("elmos %s (%s) built on %s with %s",
+		i.Version, i.Commit[:min(7, len(i.Commit))], i.BuildDate, i.GoVersion)
+}
+
+// Short returns just the version number
+func (i Info) Short() string {
+	return i.Version
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
