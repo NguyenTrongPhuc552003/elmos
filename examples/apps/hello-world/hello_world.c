d9190219a6e09390d0720c43c4bf1ee0c39fea8e
Phuc Nguyen <trong552003@gmail.com>
2026-01-12 23:37:25 +0700

    examples: apps: Move whole apps directory to new examples
    Signed-off-by: Phuc Nguyen <trong552003@gmail.com>

---
+++ examples/apps/hello-world/hello_world.c
@@ -0,0 +1,13 @@
+/*
+ * hello-world - Userspace application
+ * Cross-compiled for embedded Linux
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("hello-world: Hello from embedded Linux!\n");
+    return 0;
+}
